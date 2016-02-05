56a01efbff7e1351c82a8bd44619eaabcd3c5b4d
Bojie Jiang <jiangbojie@gmail.com>
2016-02-04 23:36:38 -0800

    To 65

---
+++ app61.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(req, res) {
+    
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+    fs.createReadStream(__dirname + '/index60.htm').pipe(res);
+}).listen(1337, '127.0.0.1');
